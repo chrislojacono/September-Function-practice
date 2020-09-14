47f733b4ac4eb02b77d42d2dbf2395c1c68933df
Chris LoJacono <clojacono92@gmail.com>
2020-09-14 17:57:54 -0500

    practice practice

--- practice.js
+++ practice.js
@@ -8,3 +8,47 @@ const BestInShow = (type) => {
 }
 const dogType = BestInShow('poodle');
 
+const calculateAgeInDogYears = (ageOfPerson) => {
+    const ageInDogYears = ageOfPerson / 7
+
+    return ageInDogYears
+}
+
+const dogAge = calculateAgeInDogYears(22)
+
+
+const add = (a, b, c) => {
+    return a + b + c
+
+}
+console.log(add(22, 34, 57))
+
+const go = (direction, speed) => {
+    if(speed > 75){
+        return `The car is moving ${direction} at ${speed}mph. SLOW DOWN!!` 
+    }
+    else{
+    return `The car is moving ${direction} at ${speed}mph`
+    }
+}
+const nums = [22, 13, 16, 53, 36, 73]
+const evenOrOdd = (num) =>{
+    if(num % 2 === 0){
+        return 'Even'
+    }
+    else{
+        return 'Odd'
+    }
+}
+const loop = (num) =>{
+    let output = ''
+    for(let i = 0; i < nums.length; i++){
+       output = evenOrOdd(num[i])
+       return output
+    }
+    
+}
+
+console.log(loop(nums))
+
+console.log(evenOrOdd(33))
